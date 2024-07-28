e0909ff6a7ac25eff585eb1323623c7a87a4b900
avinash-dubey07 <avinash.dubey2700@gmail.com>
2024-07-29 03:32:21 +0530

    Final commit

--- src/app/components/Dinner.jsx
+++ src/app/components/Dinner.jsx
@@ -77,7 +77,31 @@ const Dinner = ({ items }) => {
               style={{ zIndex: 2 }}
               className="bg-green-600 p-1 w-[70px] h-6 font-bold border-none rounded text-white cursor-pointer -mt-3"
             >
-              {isAdded(item) ? "Added" : "Add +"}
+              {isAdded(item) ? (
+                <p
+                  className="mt-0"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <span>Added</span>{" "} &nbsp;
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    class="size-4"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </p>
+              ) : (
+                "Add +"
+              )}
             </button>
           </div>
         </div>
